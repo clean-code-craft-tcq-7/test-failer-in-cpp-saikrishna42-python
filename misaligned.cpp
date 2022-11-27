c7eb5b4a845c1b3c6f03746aa38bfe614224e395
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-11-27 12:01:47 +0100

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <string>
 #include <assert.h>
 
-int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+std::string getcolor(int i,std::string colorType)
+{
+    if(colorType == "Major")
+        return majorColor[i]; 
+    else
+        return minorColor[i]; 
+}
+
+int printColorMap() {
+    
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            std::cout << i * 5 + j << " | " << getcolor(i,"Major") << " | " << getcolor(i,"Minor") << "\n";
         }
     }
     return i * j;
@@ -16,6 +27,12 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    //Test CASE 4 | White | Blue
+    // std::cout << i * 5 + j << " | " << getcolor(i,"Major") << " | " << getcolor(i,"Minor") << "\n"
+    //Sending the same value i as 4 and checking the value
+    assert(getcolor(4,"Major")=="Violet");
+    assert(getcolor(4,"Minor")=="Blue");
+ 
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
