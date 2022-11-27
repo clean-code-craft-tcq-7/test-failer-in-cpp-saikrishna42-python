76bce88abf92b148b14e3fa684c12fb7f494376e
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-11-27 13:58:30 +0100

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -19,8 +19,12 @@ void alertInCelcius(float farenheit) {
 }
 
 int main() {
+    
+    
     alertInCelcius(400.5);
+     assert(alertFailureCount==0);
     alertInCelcius(303.6);
+     assert(alertFailureCount==1);
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
