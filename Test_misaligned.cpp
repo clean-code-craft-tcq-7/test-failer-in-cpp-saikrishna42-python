f2657ac91ecd08036581b9a644c1efabedf47281
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-12-05 15:04:00 +0530

    Update Test_misaligned.cpp

--- Test_misaligned.cpp
+++ Test_misaligned.cpp
@@ -2,40 +2,55 @@
 #include "misaligned.hpp"
 
 int main() {
-    
+    int i,j,num;
     
     //Test CASE 4 | White | Blue
     // std::cout << i * 5 + j << " | " << getcolor(i,"Major") << " | " << getcolor(i,"Minor") << "\n"
     //Sending the same value i as 4 and checking the value
-    assert(getcolor(4,"Major")=="Violet");
-    assert(getcolor(4,"Minor")=="Blue");
+    num=4-1;
+    i=num/5;
+    j=num%5;
+    assert(getcolor(i,"Major")=="Violet");
+    assert(getcolor(j,"Minor")=="Blue");
   
   
   //Test CASE 8 | Red | Green
     // std::cout << i * 5 + j << " | " << getcolor(i,"Major") << " | " << getcolor(i,"Minor") << "\n"
     //Sending the same value i as 8 and checking the value
-    assert(getcolor(8,"Major")=="Red");
-    assert(getcolor(8,"Minor")=="Green");
+     num=8-1;
+    i=num/5;
+    j=num%5;
+    assert(getcolor(i,"Major")=="Red");
+    assert(getcolor(j,"Minor")=="Green");
   
   //Test CASE 15 | Black | Slate
     // std::cout << i * 5 + j << " | " << getcolor(i,"Major") << " | " << getcolor(i,"Minor") << "\n"
     //Sending the same value i as 15 and checking the value
-    assert(getcolor(15,"Major")=="Black");
-    assert(getcolor(15,"Minor")=="Slate");
+    num=15-1;
+    i=num/5;
+    j=num%5;
+    assert(getcolor(i,"Major")=="Black");
+    assert(getcolor(j,"Minor")=="Slate");
   
     
   //Test CASE 19 | Yellow | Brown
     // std::cout << i * 5 + j << " | " << getcolor(i,"Major") << " | " << getcolor(i,"Minor") << "\n"
     //Sending the same value i as 19 and checking the value
-    assert(getcolor(19,"Major")=="Yellow");
-    assert(getcolor(19,"Minor")=="Brown");
+    num=19-1;
+    i=num/5;
+    j=num%5;
+    assert(getcolor(i,"Major")=="Yellow");
+    assert(getcolor(j,"Minor")=="Brown");
   
     
   //Test CASE 25 | Violet | Slate
     // std::cout << i * 5 + j << " | " << getcolor(i,"Major") << " | " << getcolor(i,"Minor") << "\n"
     //Sending the same value i as 25 and checking the value
-    assert(getcolor(25,"Major")=="Violet");
-    assert(getcolor(25,"Minor")=="Slate");
+     num=25-1;
+    i=num/5;
+    j=num%5;
+    assert(getcolor(i,"Major")=="Violet");
+    assert(getcolor(j,"Minor")=="Slate");
   
   
  
