7e1ccdf92d24e1ed615a42b9f484162c801e7059
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-12-05 14:31:56 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <assert.h>
+
 
 char size(int cms) {
     char sizeName = '\0';
@@ -13,12 +13,4 @@ char size(int cms) {
     return sizeName;
 }
 
-int main() {
-    assert(size(37) == 'S');
-    assert(size(38) == 'M');
-    assert(size(40) == 'M');
-    assert(size(42) == 'L');
-    assert(size(43) == 'L');
-    std::cout << "All is well (maybe!)\n";
-    return 0;
-}
+
