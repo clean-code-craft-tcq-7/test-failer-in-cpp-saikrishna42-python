9d94da93ca2b1a388d5cee48ed67c717ad6d90b4
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-12-08 11:53:00 +0530

    Update test_alert.h

--- test_alert.h
+++ test_alert.h
@@ -4,7 +4,19 @@
 #include <iostream>
 #define TEST
 #ifdef TEST
-        #define networkAlert    networkAlertStub
+    int networkAlert(float celcius) 
+    {
+        std::cout << "[STUB]ALERT: Temperature is " << celcius << " celcius.\n";
+        // Return 200 for ok
+        // Return 500 for not-ok
+        // stub always succeeds and returns 200
+        if(celcius>200) {
+                return 500;
+        }
+        else{
+                return 200;
+        }
+    }
 #endif
 
 
