93a4a5e2277348c0378d0f79135228c5724071ff
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-12-08 11:40:31 +0530

    Create alerter.hpp

---
+++ alerter.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "test.hpp"
+
+#ifdef PRODUCTION
+        
+        int networkAlert(float celcius) 
+        {
+             std::cout << "[PROD]ALERT: Temperature is " << celcius << " celcius.\n";
+             return 0;
+        }
+#endif
+
+float fare_celc_conv(float farenheit)
+{
+    float celcius;
+    celcius = (farenheit - 32) * 5 / 9;
+    return celcius;
+}
