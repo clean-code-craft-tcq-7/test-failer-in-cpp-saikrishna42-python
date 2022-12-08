0ea01056c19819b7211133d3e4325c92805b7a6b
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-12-08 11:52:35 +0530

    Delete test_alert.cpp

--- test_alert.cpp
+++
@@ -1,9 +0,0 @@
-#include"test_alert.h"
- int networkAlert(float celcius) 
-    {
-        std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-        // Return 200 for ok
-        // Return 500 for not-ok
-        // stub always succeeds and returns 200
-        return 200;
-    }
