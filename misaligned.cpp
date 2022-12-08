711a76cbf251d5eb9cb8aaafb9adc32058e30347
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-12-08 15:17:09 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -4,20 +4,28 @@
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-std::string getcolor(int i,std::string colorType)
+
+std::string Map_to_string(int i,int j)
 {
-    if(colorType == "Major")
-        return majorColor[i]; 
-    else
-        return minorColor[i]; 
+    std::string str;
+    int n;
+    str=majorColor[i - 1];
+    n=str.size();
+    return std::to_string((((i - 1) * 5)+1 + (j-1))) + " \t| " + majorColor[i - 1] +std::string( 6-n, ' ' )+" | " + minorColor[j - 1];
 }
 
-void printColorMap() {
-    
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << getcolor(i,"Major") << " | " << getcolor(j,"Minor") << "\n";
+void printColorMap()
+{
+    int i = 0, j = 0,n=0;
+    std::string str;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+         
+            std::cout<<Map_to_string( i, j)<<std::endl;
+     
         }
     }
+        
 }
